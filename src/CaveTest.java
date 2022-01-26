2b57aad7b46b7573467ecedf6de96f420cc8af2c
Maverick Perrollaz <maverick.perrollaz@pm.me>
2022-01-26 16:12:03 +0100

    CaveTest

--- src/CaveTest.java
+++ src/CaveTest.java
@@ -2,23 +2,48 @@ package src;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CaveTest {
 
-  protected void setUp() {
+  private Cave cave;
 
+  @BeforeEach
+  void setUp() {
+    cave = new Cave();
+    cave.add(new Boisson("testBoisson1"));
+    cave.add(new Boisson("testBoisson2"));
+    cave.add(new Boisson("testBoisson3"));
+    cave.add(new Boisson("testBoisson3"));
+    cave.add(new Boisson("testBoisson3"));
   }
 
   @Test
-  void add() {
+  void testAdd() {
+    Boisson boissonAdd = new Boisson("testBoisson");
+    cave.add(boissonAdd);
+    assertEquals(boissonAdd, cave.rayons.lastElement());
   }
 
   @Test
-  void take() {
+  void testTake() {
+    assertEquals("testBoisson1", cave.take("testBoisson1").nom);
   }
 
   @Test
   void testToString() {
+    System.out.println(cave.toString());
+
+    String eol = System.getProperty("line.separator");
+
+    String result = "Cave : \n"
+        + "\ttestBoisson1\n"
+        + "\ttestBoisson2\n"
+        + "\ttestBoisson3\n"
+        + "\ttestBoisson3\n"
+        + "\ttestBoisson3\n";
+
+    assertEquals(cave.toString(), result);
   }
 }
