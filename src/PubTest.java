172161fe098c3e26e4668d5c6d339aff34ff55bb
Maverick Perrollaz <maverick.perrollaz@pm.me>
2022-01-26 16:58:22 +0100

    PubTest by @fredgig

--- src/PubTest.java
+++ src/PubTest.java
@@ -1,15 +1,27 @@
 package src;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PubTest {
 
-  @org.junit.jupiter.api.Test
-  void approvisionnerBar() {
+  private Pub pub;
 
+
+  @BeforeEach
+  void setUp() {
+    pub = new Pub();
   }
 
-  @org.junit.jupiter.api.Test
-  void main() {
+  @Test
+  void testApprovisionnerBar() {
+    Boisson boisson = new Boisson("Cola");
+    pub.cave.add(boisson);
+    pub.approvisionnerBar("Cola");
+
+    assertEquals(0, pub.cave.rayons.size());
+    assertEquals(1, pub.bar.boissonFroide.size());
   }
 }
